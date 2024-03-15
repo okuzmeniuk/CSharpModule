11a4d3451f5757e05e2e3fc1f19f34a44900faa3
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-03-15 10:41:33 +0200

    Upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace ModuleN1
 {
+    // Після створення класу запустіть програму,
+    // яка створює масив,
+    // знаходить найбільший елемент та сортує масив.
+    // Виведіть початковий масив, знайдений максимум та відсортований масив на консоль.
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int[] array = ArrayManipulator.GenerateRandomArray(10, -5, 5);
+            int maxElement = ArrayManipulator.FindMax(array);
+            int[] sortedArray = ArrayManipulator.SortArray(array);
+
+            Console.WriteLine("Initial array:");
+            ArrayManipulator.PrintArray(array);
+            Console.WriteLine($"Max element in array: {maxElement}");
+            Console.WriteLine("Sorted array:");
+            ArrayManipulator.PrintArray(sortedArray);
         }
     }
 }
