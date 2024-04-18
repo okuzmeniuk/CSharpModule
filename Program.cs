d5a656c5ceea31fdd2a31f5dac2526d8a8df46ea
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-04-18 23:44:08 +0300

    Undo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using CSharpModules.Module1;
-
-namespace CSharpModules
+﻿namespace ModuleN1
 {
-    public class Program
-    {
+	// Після створення класу запустіть програму,
+	// яка створює масив,
+	// знаходить найбільший елемент та сортує масив.
+	// Виведіть початковий масив, знайдений максимум та відсортований масив на консоль.
+	internal class Program
+	{
 		static void Main(string[] args)
 		{
-			Module1Runner.Run();
+
+			int[] array = ArrayManipulator.GenerateRandomArray(10, -5, 5);
+			int maxElement = ArrayManipulator.FindMax(array);
+			int[] sortedArray = ArrayManipulator.SortArray(array);
+
+			Console.WriteLine("Initial array:");
+			ArrayManipulator.PrintArray(array);
+			Console.WriteLine($"Max element in array: {maxElement}");
+			Console.WriteLine("Sorted array:");
+			ArrayManipulator.PrintArray(sortedArray);
 		}
 	}
 }
